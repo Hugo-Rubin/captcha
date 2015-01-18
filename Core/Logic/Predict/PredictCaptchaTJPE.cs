a8da772444df2dc15e9a768f47a0a6f7425e7b36
Pablo Silva <pablo.carvalho@gmail.com>
2015-01-18 15:55:49 +0000

    PredictCaptchaTJPE and PredictCaptchaTRTSP fixed

--- Core/Logic/Predict/PredictCaptchaTJPE.cs
+++ Core/Logic/Predict/PredictCaptchaTJPE.cs
@@ -3,43 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Core.Logic.Predict.Abstract;
+using Core.Logic.Captchas;
 
 namespace Core.Logic.Predict
 {
-    class PredictCaptchaTJPE
+    public class PredictCaptchaTJPE : PredictNeuralNetwork<PredictCaptchaTJPE>
     {
-
-        public class PredictCaptchaTJPE : PredictNeuralNetwork<PredictCaptchaTJPE>
+        protected override int HiddenUnits
         {
-            protected override int HiddenUnits
-            {
-                get { return 234; }
-            }
+            get { return 234; }
+        }
 
-            protected override int TamanhoCaracter
-            {
-                get { return 4225; }
-            }
+        protected override int TamanhoCaracter
+        {
+            get { return 4225; }
+        }
 
-            protected override char[] Dicionario
+        protected override char[] Dicionario
+        {
+            get
             {
-                get
-                {
-                    return new[]
+                return new[]
                             {
                                 '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'b' ,'B', 'c',
                                 'd', 'D', 'e', 'E', 'f', 'F', 'g', 'G', 'h', 'H', 'i', 'j', 'J',
                                 'k', 'l', 'L', 'm', 'M', 'n', 'N', 'o', 'O', 'p', 'q', 'Q', 'r',
                                 'R', 's', 't', 'T', 'u', 'U', 'v', 'w', 'W', 'x', 'y', 'Y', 'z'
                             };
-                }
             }
+        }
 
-            public override string SiglaServico
-            {
-                get { return "TJPE"; }
+        public override string SiglaServico
+        {
+            get { return "TJPE"; }
 
-            }
         }
     }
 }
--- Core/Logic/Predict/PredictCaptchaTRTSP.cs
+++ Core/Logic/Predict/PredictCaptchaTRTSP.cs
@@ -3,43 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Core.Logic.Predict.Abstract;
+using Core.Logic.Captchas;
 
 namespace Core.Logic.Predict
 {
-    class PredictCaptchaTRTSP
+    public class PredictCaptchaTRTSP : PredictNeuralNetwork<PredictCaptchaTRTSP>
     {
-
-        public class PredictCaptchaTRTSP : PredictNeuralNetwork<PredictCaptchaTRTSP>
+        protected override int HiddenUnits
         {
-            protected override int HiddenUnits
-            {
-                get { return 149; }
-            }
+            get { return 149; }
+        }
 
-            protected override int TamanhoCaracter
-            {
-                get { return 3600; }
-            }
+        protected override int TamanhoCaracter
+        {
+            get { return 3600; }
+        }
 
-            protected override char[] Dicionario
+        protected override char[] Dicionario
+        {
+            get
             {
-                get
-                {
-                    return new[]
+                return new[]
                             {
                                 '2', '3', '4', '5', '6', '7', '8', '9', 'a',
                                 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
                                 'k', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
                                 'u', 'v', 'w', 'x', 'y', 'z'
                             };
-                }
-            }
-
-            public override string SiglaServico
-            {
-                get { return "TRTSP"; }
-
             }
         }
+
+        public override string SiglaServico
+        {
+            get { return "TRTSP"; }
+
+        }
     }
+
 }
