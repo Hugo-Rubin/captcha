2fa06ba9628cb188ecd5f7c17fefcc47f53a9a3c
Hugo <hug0liveira@globo.om>
2015-02-24 09:36:49 -0300

    Tratamento do PJE 100% concluído.

--- Core/Logic/Captchas/CaptchaPJE.cs
+++ Core/Logic/Captchas/CaptchaPJE.cs
@@ -44,18 +44,25 @@ namespace Core.Logic.Captchas
 
         public override IEnumerable<ImgArray> GetCaracteres()
         {
-            SeparacaoPadrao sp = new SeparacaoPadrao(this);
-            return sp.ColorFillingSegmentation2AndSeamCarving2(null, false).CortarECentralizarTodos(TamanhoImagemLetra.X, TamanhoImagemLetra.Y);
+            // Como as imagens possuem tamanho padronizado, podemos fazer a separação dos caracteres com cortes retos entre as letras
+            List<ImgArray> letras = new List<ImgArray>();
+            letras.Add(this.ImgArray.GetSegment(new Rectangle( 0, 0, 15, this.ImgArray.Height)));
+            letras.Add(this.ImgArray.GetSegment(new Rectangle(17, 0, 15, this.ImgArray.Height)));
+            letras.Add(this.ImgArray.GetSegment(new Rectangle(33, 0, 15, this.ImgArray.Height)));
+            letras.Add(this.ImgArray.GetSegment(new Rectangle(49, 0, 15, this.ImgArray.Height)));
+            letras.Add(this.ImgArray.GetSegment(new Rectangle(65, 0, 14, this.ImgArray.Height)));
+
+            return letras.CortarECentralizarTodos(TamanhoImagemLetra.X, TamanhoImagemLetra.Y);
         }
 
         public override Bitmap RemoverFundo(Bitmap source)
         {
             ImgArray img = new ImgArray(source, false, 212);
             
-            // Máscaras [0, 1, 0] e [0] são usadas para remover os quadriláteros da imagem. Eles tem 1 pixel de espessura e a máscara precisa ser aplicada na vertical e na horizontal.
-            //                      [1]
-            //                      [0]
-
+            // As máscaras [0, 1, 0] e [0] são usadas para remover os quadriláteros da imagem. Eles tem 1 pixel de espessura e a máscara precisa ser aplicada na vertical e na horizontal.
+            //                         [1]
+            //                         [0]
+            
             // Máscara horizontal:
             for (int y = 0; y < img.Height; y++)
             {
@@ -80,7 +87,7 @@ namespace Core.Logic.Captchas
                 }
             }
 
-            return img.ApagarMargem().ToBitmap();
+            return img.GetSegment(new Rectangle(12, 13, 78, 22)).ToBitmap(); // Corta a imagem nas bordas do texto
         }
 
 
